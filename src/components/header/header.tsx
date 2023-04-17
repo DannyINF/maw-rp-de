668fcfaae00e517dac4899d43d6114799e6d5e0a
realVincxnt2 <stuff.vincxnt2@gmail.com>
2023-04-17 09:24:22 +0200

    feed(header) added logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,30 +1,44 @@
 // components/Header.js
 
-import React from 'react';
-import styles from './header.module.scss';
+import React from "react";
+import styles from "./header.module.scss";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <a href="#home" className={styles.link}>Mythen aus Westernis</a>
+        <Image src="/logo-black.png" alt="logo" className={styles.logo} width={35} height={35} />
+        <a href="#home" className={styles.link}>
+          Mythen aus Westernis
+        </a>
       </div>
       <nav className={styles.nav}>
         <ul>
           <li>
-            <a href="#geschichte" className={styles.link}>Geschichte</a>
+            <a href="#geschichte" className={styles.link}>
+              Geschichte
+            </a>
           </li>
           <li>
-            <a href="#regeln" className={styles.link}>Regeln</a>
+            <a href="#regeln" className={styles.link}>
+              Regeln
+            </a>
           </li>
           <li>
-            <a href="#rassen" className={styles.link}>Rassen</a>
+            <a href="#rassen" className={styles.link}>
+              Rassen
+            </a>
           </li>
           <li>
-            <a href="#fraktionen" className={styles.link}>Fraktionen</a>
+            <a href="#fraktionen" className={styles.link}>
+              Fraktionen
+            </a>
           </li>
           <li>
-            <a href="#beitreten" className={styles.link}>Beitreten</a>
+            <a href="#beitreten" className={styles.linkMarked}>
+              Beitreten
+            </a>
           </li>
         </ul>
       </nav>
