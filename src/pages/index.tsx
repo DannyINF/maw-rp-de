592df7f5f73c27ca67d7bff6cc0f30279f710f13
Mekphen <Fabian@Zentros-Gaming.de>
2023-04-17 09:37:40 +0200

    feat(header) design improvement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import Header from '@/components/header/header'
-import Hero from '@/components/hero/hero'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import Header from '@/components/header/header';
+import Hero from '@/components/hero/hero';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Mythen aus Westernis RP - Deine einzigartige Rollenspiel Erfahrung in Garry's Mod</title>
-        <meta name="description" content="Tritt noch heute dem Mythen aus Westernis Rollenspiel-Server bei und lerne als Mensch, Zwerg oder Ork eine neue Welt kennen!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Header />
-        <Hero />
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>
+					Mythen aus Westernis RP - Deine einzigartige Rollenspiel
+					Erfahrung in Garry's Mod
+				</title>
+				<meta
+					name="description"
+					content="Tritt noch heute dem Mythen aus Westernis Rollenspiel-Server bei und lerne als Mensch, Zwerg oder Ork eine neue Welt kennen!"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<Header />
+				<Hero />
+			</main>
+		</>
+	);
 }
