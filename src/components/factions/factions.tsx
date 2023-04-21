90516fa6f7237cac452234e70f737ba0c2c92d6b
Mekphen <Fabian@Zentros-Gaming.de>
2023-04-21 22:10:38 +0200

    feat(global) hide overflow-x on body

--- src/components/factions/factions.tsx
+++ src/components/factions/factions.tsx
@@ -1,5 +1,5 @@
 // Factions.tsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './factions.module.scss';
 import Image from 'next/image';
 
@@ -25,11 +25,52 @@ interface FactionsProps {
 }
 
 const Factions: React.FC<FactionsProps> = ({ factions }) => {
+	const factionsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			factionsRef.current.forEach((faction, index) => {
+				if (faction) {
+					const rect = faction.getBoundingClientRect();
+					const windowHeight = window.innerHeight;
+					if (rect.top < windowHeight && rect.bottom > 0) {
+						const progress =
+							1.3 - Math.max(0, rect.top) / (windowHeight * 1);
+						const translation = (1 - progress) * 500;
+						const direction = index % 2 === 0 ? 1 : -1;
+						const finalTranslation = translation * direction;
+
+						if (direction === 1) {
+							faction.style.transform = `translateX(${
+								(finalTranslation > 0 && finalTranslation) || 0
+							}px)`;
+						} else {
+							faction.style.transform = `translateX(${
+								(finalTranslation < 0 && finalTranslation) || 0
+							}px)`;
+						}
+					}
+				}
+			});
+		};
+
+		window.addEventListener('scroll', handleScroll);
+		handleScroll(); // Um die Anfangstransparenz zu initialisieren
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
 		<section id="fraktionen" className={styles.factions}>
 			<h2>Fraktionen</h2>
 			{factions.map((faction, index) => (
-				<div key={index} className={styles.faction}>
+				<div
+					key={index}
+					className={`${styles.faction}`}
+					ref={element => (factionsRef.current[index] = element)}
+				>
 					<div className={styles.image}>
 						<Image
 							width={500}
