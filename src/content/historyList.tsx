5de4eb6e79802819e7b6cc12cedce6dfc1972ffa
Mekphen <Fabian@Zentros-Gaming.de>
2023-04-24 22:07:08 +0200

    feat(history) content

--- src/content/historyList.tsx
+++ src/content/historyList.tsx
@@ -1,31 +1,34 @@
 import { HistoryItem } from '@/components/history/history';
-import { FaCode, FaCogs, FaBug, FaPencilAlt } from 'react-icons/fa';
+import { FaCode, FaCogs, FaBug, FaPencilAlt, FaHandshake } from 'react-icons/fa';
 import { BsFillRocketTakeoffFill } from 'react-icons/bs';
+import { TbChristmasTree } from 'react-icons/tb';
+import { GiFireworkRocket, GiFireBottle } from 'react-icons/gi';
+import { RiSwordLine } from 'react-icons/ri';
 
 export const historyList: HistoryItem[] = [
 	{
 		key: 1,
-		date: '01. Januar 2022',
+		date: 'Februar 2022',
 		title: 'Konzeptionierung',
 		icon: <FaPencilAlt role="img" aria-label="Pencil" />,
 		content:
-			'Die Idee für den Herr der Ringe Rollenspiel-Server wird geboren. Das Team beginnt mit der Planung und Konzeptionierung des Projekts.',
+			'Die Idee für den Herr der Ringe Rollenspiel-Server wird geboren. Die Serverleiter beginnen mit der Planung und der Konzeptionierung des Servers und der Systeme.',
 	},
 	{
 		key: 2,
-		date: '15. März 2022',
+		date: 'Juni 2022',
 		title: 'Entwicklung beginnt',
 		icon: <FaCode role="img" aria-label="Code" />,
 		content:
-			'Das Entwicklerteam startet die Umsetzung der geplanten Features und die Erstellung des Spielservers.',
+			'Mekphen startet als Entwickler die Umsetzung der geplanten Features und die Erstellung des Spielservers. Dabei entstehen die ersten Bausteine wie Inventar System und Crafting System.',
 	},
 	{
 		key: 3,
-		date: '01. September 2022',
+		date: 'September 2022',
 		title: 'Beta-Test',
 		icon: <FaBug role="img" aria-label="Bug" />,
 		content:
-			'Der Server wird für einen geschlossenen Beta-Test geöffnet. Eine begrenzte Anzahl von Testern erhält Zugang, um Feedback zu geben und Fehler zu melden.',
+			'Der Server wird für einen geschlossenen Beta-Test geöffnet. Eine begrenzte Anzahl von Testern erhält Zugang, um die Systeme und Features des Servers zu testen und Feedback an das Team zu geben.',
 	},
 	{
 		key: 4,
@@ -37,10 +40,50 @@ export const historyList: HistoryItem[] = [
 	},
 	{
 		key: 5,
-		date: '15. November 2022',
-		title: 'Update 1.1',
+		date: 'November 2022',
+		title: 'Erstes Update',
 		icon: <FaCogs role="img" aria-label="Cogs" />,
 		content:
-			'Das erste große Update wird veröffentlicht, das neue Funktionen, Fehlerbehebungen und Verbesserungen enthält.',
+			'Das erste große Update wird veröffentlicht. Es gibt nun ein Hunger-System, Eroberungsgebiete haben einen Cooldown, Moria bekommt einen Zwergenfürst und etliche neue Verbesserungen erscheinen auf dem Server.',
+	},
+	{
+		key: 6,
+		date: 'Dezember 2022',
+		title: 'Dezember Update',
+		icon: <TbChristmasTree role="img" aria-label="Christmas Tree" />,
+		content:
+			'In dem riesigen Dezember Update kommen Features wie das Trainings-System, die Möglichkeit zum Kauf von Häusern und vieles mehr. Außerdem bekommen Moria, Gondor und Angmar eine besondere Spezialeinheit.',
+	},
+	{
+		key: 7,
+		date: 'Januar 2022',
+		title: 'Eventwoche',
+		icon: <GiFireworkRocket role="img" aria-label="Firework Rocket" />,
+		content:
+			'Bereits am Anfang des neuen Jahres fand eine große Eventwoche statt. Angmar wird zu Isengard und später zu Mordor. Moria wird erst ein Teil von Gondor und danach zu Rohan. Ebenfalls weren große Schlachten, wie die von Minas Tirith und Helms Klamm nachgespielt.',
+	},
+	{
+		key: 8,
+		date: 'März 2022',
+		title: 'Krieg von Westernis',
+		icon: <RiSwordLine role="img" aria-label="Swords" />,
+		content:
+			'Mit Krieg von Westernis kommt das größte alleinstehende Feature auf den Server. Durch über 10.000 Zeilen Code schafft es Krieg von Westernis ein eigenes Strategiespiel für die Führungsebenen zu werden.',
+	},
+	{
+		key: 9,
+		date: 'März 2022',
+		title: 'Alchemie System',
+		icon: <GiFireBottle role="img" aria-label="Fire Bottle" />,
+		content:
+			'Durch das neue Alchemie System werden die Kräuter vervielfacht und die Alchemisten haben die Möglichkeit, ihr können mit neuem und einzigartigem Gameplay auf die Probe zu stellen. Neben den neuen Kräutern gibt es auch viele neue Tränke, welche in der richtigen Reihenfolge gebraut werden müssen.',
+	},
+	{
+		key: 10,
+		date: 'April 2022',
+		title: 'Arost Bündnis',
+		icon: <FaHandshake role="img" aria-label="Handshake" />,
+		content:
+			'Nach gemeinsamer Entscheidung schließen sich die 3 Nebenfraktionen der Elben, Dunländer und Wandernden zusammen und werden zum Bündnis der neutralen Handelsstadt Arost. Die Stadt wird zu einem wichtigen Handelszentrum und wird von allen Fraktionen als neue Hauptstadt anerkannt.',
 	},
 ];
