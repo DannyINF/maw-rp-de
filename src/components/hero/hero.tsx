5c99213653b0cf8f0fb86eadc0a4205cbd07e6a3
Mekphen <Fabian@Zentros-Gaming.de>
2023-04-24 10:29:26 +0200

    feat(hero) design improvements

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,19 +4,27 @@ import styles from './hero.module.scss';
 
 const Hero = () => {
 	return (
-		<section tabIndex={0} role="article">
+		<section tabIndex={0} role="article" className={styles.heroWrapper}>
 			<Parallax
 				blur={{ min: 2, max: 2 }}
 				bgImage="/assets/images/hero-bg1.webp"
 				bgImageAlt="background"
 				bgClassName={styles.bgImage}
-				strength={200}
+				strength={500}
 			>
 				<div className={styles.hero}>
 					<h1 className={styles.headline}>Mythen aus Westernis</h1>
 					<h2 className={styles.subheadline}>
 						Deine Bestimmung, in der großen von Westernis Welt
 					</h2>
+					<div className={styles.heroButtons}>
+						<a href="#fraktionen" className={styles.buttonPrimary}>
+							Fraktionen
+						</a>
+						<a href="#beitreten" className={styles.buttonSecondary}>
+							Beitreten
+						</a>
+					</div>
 				</div>
 			</Parallax>
 		</section>
